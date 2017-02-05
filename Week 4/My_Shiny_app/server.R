df7e8caf71614280458e04ad74ad3e7cef0b479c
marichards <marichards88@gmail.com>
2017-02-05 10:13:45 -0800

    Finished Shiny App

--- Week 4/My_Shiny_app/server.R
+++ Week 4/My_Shiny_app/server.R
@@ -1,10 +1,3 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(leaflet)
 
@@ -13,11 +6,20 @@ shinyServer(function(input, output) {
   # Create the data frame of all locations
   seattle_loc <- data.frame(
     lat = c(47.60972,47.5994409,47.6816841,47.6611935,
-            47.67538,47.6577795,47.6225807,47.6861061),
+            47.67538,47.6577795,47.6225807,47.6861061,
+            47.6503061,47.651112,47.6719757,47.639056,
+            47.598996,47.6107739,47.6495925,47.601597,
+            47.652196,47.6195659,47.6329571,47.6178717),
     lng = c(-122.344387,-122.3312286,-122.3786947,-122.3422866,
-            -122.327614,-122.3197193,-122.2994758,-122.3403572),
+            -122.327614,-122.3197193,-122.2994758,-122.3403572,
+            -122.3523452,-122.3454357,-122.390061,-122.3588257,
+            -122.3348717,-122.3503437,-122.3445494,-122.3344979,
+            -122.3549098,-122.3150591,-122.3196533,-122.3285566),
     col = c("blue", "red", "red","purple",
-            "green","green","purple","blue"),
+            "green","green","purple","blue",
+            "orange","black","black","orange",
+            "red","blue","purple","red",
+            "green","black","orange","black"),
     stringsAsFactors = FALSE,
     sites <- c(
     "<a href='http://pikeplacemarket.org/'>Pike Place Market</a>",
@@ -27,27 +29,44 @@ shinyServer(function(input, output) {
     "<a href='http://www.butcherbakerseattle.com/'>The Butcher & The Baker</a>",
     "<a href='http://www.portagebaycafe.com/'>Portage Bay Cafe</a>",
     "<a href='http://www.harvestvine.com/'>The Harvest Vine</a>",
-    "<a href='http://www.dukeschowderhouse.com/locations/green-lake/'>Duke's Chowder House</a>"
-  ))
-  
-
-  
-  location.selector <- reactive({
-    #input$update # Update if the button is clicked
-    # Create the color and labels data frame
-    legend.info <- data.frame(labels =  c("Seafood",
-                                          "Sandwiches",
-                                          "Brunch",
-                                          "Upscale"),
-                              colors = c("blue",
-                                         "red",
-                                         "green",
-                                         "purple"))
+    "<a href='http://www.dukeschowderhouse.com/locations/green-lake/'>Duke's Chowder House</a>",
+    "<a href='https://www.zomato.com/seattle/jai-thai-fremont'>Jai Thai</a>",
+    "<a href='http://www.seawolfbakers.com/'>Sea Wolf Bakers</a>",
+    "<a href='http://cafebesalu.com/'>Cafe Besalu</a>",
+    "<a href='https://www.zomato.com/seattle/ying-thai-kitchen-queen-anne-upper'>Thai Kitchen</a>",
+    "<a href='http://rainshadowmeats.com/'>Rain Shadow Meats</a>",
+    "<a href='https://www.anthonys.com/restaurants/detail/anthonys-pier-66'>Anthony's Pier 66</a>",
+    "<a href='http://www.thewhalewins.com/'>The Whale Wins</a>",
+    "<a href='http://www.tatsdeli.com/'>Tat's Deli</a>",
+    "<a href='https://www.pastramisandwich.com/'>Roxy's Diner</a>",
+    "<a href='http://www.bakerynouveau.com/'>Bakery Nouveau</a>",
+    "<a href='http://wedgwoodtwovegetarianthai.com/'>Wedgewood II Vegetarian Thai</a>",
+    "<a href='https://www.crumbleandflake.com/'>Crumble and Flake</a>"
     
+  ))
+  # Create the color and labels data frame
+  legend.info <- data.frame(labels =  c("Seafood",
+                                        "Sandwiches",
+                                        "Brunch",
+                                        "Upscale",
+                                        "Thai",
+                                        "Bakeries"),
+                            colors = c("blue",
+                                       "red",
+                                       "green",
+                                       "purple",
+                                       "orange",
+                                       "black"))  
+
+  # Make a reactive expression that filters locations
+  location.selector <- reactive({
+    #input$update.map # Update if the button is clicked
     button.logic <- c(input$seafood,
                       input$sandwiches,
                       input$brunch,
-                      input$upscale)
+                      input$upscale,
+                      input$thai,
+                      input$bakery)
     final.legend <- legend.info[button.logic,]
     final.locations <- subset(seattle_loc, col %in% final.legend$colors)
     return(list(final.legend,final.locations))
--- Week 4/My_Shiny_app/ui.R
+++ Week 4/My_Shiny_app/ui.R
@@ -1,10 +1,3 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(leaflet)
 
@@ -16,19 +9,26 @@ shinyUI(fluidPage(
   # Sidebar with a Check box input 
   sidebarLayout(
     sidebarPanel(
+      h2("Choose Your Filters"),
+      p("(Please check at least 1 box)"),
       checkboxInput("brunch","Show Brunch", value = TRUE),
       checkboxInput("upscale","Show Upscale", value = TRUE),
       checkboxInput("seafood","Show Seafood", value = TRUE),
       checkboxInput("sandwiches","Show Sandwiches", value = TRUE),
+      checkboxInput("thai", "Show Thai", value = TRUE),
+      checkboxInput("bakery", "Show Bakeries", value = TRUE),
       submitButton("Update Map")
     ),
     mainPanel(
-      leafletOutput("map1")
+      leafletOutput("map1"),
+      h2("Instructions for Use"),
+      tags$ol(
+        tags$li("Select your restaurant filters using the checkboxes"),
+        tags$li("Click 'Update Map' to apply the filters and update the map"),
+        tags$li("Use the map to zoom in and out"),
+        tags$li("Click on a colored marker to display the restaurant name, which links to its website")
       )
-    
-                  
+      )
     )
-
-    
   )
 )
